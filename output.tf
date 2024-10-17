5c4631c3ff96c24ef95cf9e0019eec0547f27bd5
umavanth21 <56533761+umavanth21@users.noreply.github.com>
2024-10-17 22:33:02 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,32 +1,14 @@
-output "virtual_network_name" {
-  description = "The name of the virtual network."
-  value       = azurerm_virtual_network.example.name
+output "resource_group_name" {
+  description = "The name of the resource group."
+  value       = azurerm_resource_group.example.name
 }
 
-output "subnet_id" {
-  description = "The ID of the created subnet."
-  value       = azurerm_subnet.example.id
+output "virtual_machine_id" {
+  description = "The ID of the created virtual machine."
+  value       = azurerm_virtual_machine.example.id
 }
 
-
-
-output "public_ip_address" {
-  description = "The public IP address of the virtual machine (if applicable)."
-  value       = azurerm_public_ip.example.ip_address
-  
-}
-
-output "private_ip_address" {
-  description = "The private IP address of the virtual machine."
-  value       = azurerm_network_interface.example.private_ip_address
-}
-
-output "image_id" {
-  description = "The ID of the image used to create the virtual machine."
-  value       = var.image_id
-}
-
-output "location" {
-  description = "The location (region) where resources are created."
-  value       = azurerm_resource_group.example.location
+output "network_interface_id" {
+  description = "The ID of the network interface."
+  value       = azurerm_network_interface.example.id
 }
